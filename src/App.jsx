e9df9bd84fbec2f5276c00e0e71548dd798218ec
Rob Sassack <rsassack25@gmail.com>
2022-12-22 22:26:05 -0500

    Added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
+import { NavLink, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+import Cart from "./pages/Cart";
+import NotFound from "./pages/NotFound";
 import "./App.css";
 
 function App() {
   return (
     <div className='App'>
-      <h1>Shopping Cart</h1>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to='/'>Computer World</NavLink>
+          </li>
+          <li>
+            <NavLink to='/shop'>Shop</NavLink>
+          </li>
+          <li>
+            <NavLink to='/cart'>Cart</NavLink>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/shop' element={<Shop />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
