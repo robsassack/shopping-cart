42632fd5f9d8613e9ca5ed02710f8f2be599a1ca
Rob Sassack <rsassack25@gmail.com>
2022-12-30 21:48:23 -0500

    Added styling to cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -21,29 +21,35 @@ function Cart(props) {
   let displayCart = props.cart.map((item) => {
     let itemInfo = parts.list.find((part) => part.id === item.id);
     return (
-      <div key={item.id}>
-        <li>
-          {itemInfo.name} - x{item.quantity} - ${itemInfo.price}
-        </li>
-        <button onClick={() => props.addToCart(item)}>+</button>
-        <button onClick={() => props.removeFromCart(item)}>-</button>
-        <button onClick={() => props.removeItem(item)}>X</button>
+      <div key={item.id} className='cart--item'>
+        <img src={itemInfo.image} alt={itemInfo.name} className='cart--image' />
+        <div className='cart--info'>
+          <p className="cart--name">
+            {itemInfo.name} - <strong>${itemInfo.price}</strong>
+          </p>
+          <div className='cart--quantity'>
+            <button onClick={() => props.removeFromCart(item)}>-</button>
+            <strong>{item.quantity}</strong>
+            <button onClick={() => props.addToCart(item)}>+</button>
+            <button onClick={() => props.removeItem(item)} className="cart--delete">X</button>
+          </div>
+        </div>
       </div>
     );
   });
 
   return (
-    <div>
+    <div className="cart">
       <h1>Shopping Cart</h1>
       {props.cart.length === 0 ? (
         <>
-          <p>Your cart is empty</p>
+          <p className="cart--empty"><strong>Your cart is empty</strong></p>
         </>
       ) : (
         <>
           <ul>{displayCart}</ul>
           <h2>Total: ${total}</h2>
-          <button>Checkout</button>
+          <button className="cart--checkout">Checkout</button>
         </>
       )}
     </div>
